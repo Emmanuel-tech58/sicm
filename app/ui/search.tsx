834b7060934f2316e9b00ba424a13786a8ad9768
emmanuel-tech58 <esngwala@gmail.com>
2024-01-23 14:35:53 +0200

    Added adding of invoices

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -20,6 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
